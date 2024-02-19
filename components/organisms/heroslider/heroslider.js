000d83ce43efbcea6ec8e074e93d71640c02eb11
Rajab Natshah <rajabn@gmail.com>
2024-02-19 19:49:02 +0300

    Issue #3421069: Add Event Listener for slide.bs.carousel and slid.bs.carousel to load Drimage Image for the slide

--- components/organisms/heroslider/heroslider.js
+++ components/organisms/heroslider/heroslider.js
@@ -4,6 +4,12 @@
       $(".carousel-control-pause").click((function() {
         $($(this).data("bs-target")).carousel("pause");
       }));
+      const varbase_heroslider_carousel = document.getElementById("varbase_heroslider");
+      varbase_heroslider_carousel.addEventListener("slide.bs.carousel", (event => {
+        Drupal.drimage.init(document);
+      })), varbase_heroslider_carousel.addEventListener("slid.bs.carousel", (event => {
+        Drupal.drimage.init(document);
+      }));
     }
   };
 }(jQuery, Drupal);
