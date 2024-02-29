38aef714e3bf19ecd23ff6e32de389925b035a81
Rajab Natshah <rajabn@gmail.com>
2024-02-29 18:18:15 +0300

    Issue #3421069: Add default Drupal dependencies for Varbase Components - Bootstrap Base component script to be able to pass drupal settings, once, and jquery to custom behaviors

--- components/organisms/heroslider/heroslider.js
+++ components/organisms/heroslider/heroslider.js
@@ -3,35 +3,73 @@
  * JavaScript behaviors for Varbase Hero Slider.
  */
 
-(function (Drupal, once, bootstrap) {
+(function ($, Drupal, once) {
 
   'use strict';
 
   /**
-   * Varbase Hero Slider Pause administration.
+   * Varbase Hero Slider Pause behaviors.
    *
    * @type {Drupal~behavior}
+   * 
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches behavior for pausing or sliding.
    */
   Drupal.behaviors.varbaseHeroSliderPause = {
-    attach: function (context) {
-      once("pause-heroslider", ".carousel-control-pause", context).forEach((element => {
-        element.addEventListener("click", (e => {
-          bootstrap.Carousel.getInstance(document.querySelector(element.dataset.bsTarget)).pause();
-        }));
-      }));
+    attach: function (context, settings) {
+
+      // When clicking on the pause button.
+      $(
+        once(
+          'varbase-heroslider-pause',
+          '.js-carousel-control-pause',
+          context,
+        ),
+      )
+      .on('click', (event) => {
+        const target_heroslider = $($(event.currentTarget).data('bs-target'));
+        target_heroslider.carousel({
+          interval: target_heroslider.data('bs-interval'),
+          pause: false,
+        });
+        target_heroslider.carousel('pause');
+        event.preventDefault();
+        event.stopPropagation();
+      });
+
+      // When clicking on next slide or previous slide buttons.
+      // reset the pause to on the default data-bs-pause value (hover)
+      // and slide.
+      $(
+        once(
+          'varbase-heroslider-carousel',
+          '.js-carousel-control-next , .js-carousel-control-prev',
+          context,
+        ),
+      ).on('click', (event) => {
+        const target_heroslider = $($(event.currentTarget).data('bs-target'));
+        target_heroslider.carousel({
+          interval: target_heroslider.data('bs-interval'),
+          pause: target_heroslider.data('bs-pause'),
+        });
+        target_heroslider.carousel($(event.currentTarget).data('bs-slide'));
+      });
     }
   };
 
   /**
-   * Varbase Hero Slider integration with drimage.
+   * Varbase Hero Slider integration with drimage behaviors.
    *
    * @type {Drupal~behavior}
+   * 
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches behavior for generating an image with view width and height using drimage.
    */
   Drupal.behaviors.varbaseHeroSliderDrimage = {
-    attach: function (context) {
+    attach: function (context, settings) {
       document.querySelector(".carousel.varbase-heroslider", context).addEventListener("slid.bs.carousel", (event => {
         Drupal.drimage.init(document.querySelector(".carousel.varbase-heroslider", context));
       }));
     }
   };
-})(Drupal, once, bootstrap);
+})(jQuery, Drupal, once);
