2e0705e12d900d11291a738e9b8364b7aec3d0b6
Rajab Natshah <rajabn@gmail.com>
2024-02-29 15:06:10 +0300

    Issue #3421069: Add default Drupal dependencies for Varbase Components - Bootstrap Base component script to be able to pass drupal settings, once, and jquery to custom behaviors

--- components/organisms/heroslider/heroslider.js
+++ components/organisms/heroslider/heroslider.js
@@ -1,17 +1,37 @@
-!function(Drupal, once) {
+/**
+ * @file
+ * JavaScript behaviors for Varbase Hero Slider.
+ */
+
+(function (Drupal, once, bootstrap) {
+
+  'use strict';
+
+  /**
+   * Varbase Hero Slider Pause administration.
+   *
+   * @type {Drupal~behavior}
+   */
   Drupal.behaviors.varbaseHeroSliderPause = {
-    attach: function(context) {
+    attach: function (context) {
       once("pause-heroslider", ".carousel-control-pause", context).forEach((element => {
         element.addEventListener("click", (e => {
           bootstrap.Carousel.getInstance(document.querySelector(element.dataset.bsTarget)).pause();
         }));
       }));
     }
-  }, Drupal.behaviors.varbaseHeroSliderDrimage = {
-    attach: function(context) {
+  };
+
+  /**
+   * Varbase Hero Slider integration with drimage.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.varbaseHeroSliderDrimage = {
+    attach: function (context) {
       document.querySelector(".carousel.varbase-heroslider", context).addEventListener("slid.bs.carousel", (event => {
         Drupal.drimage.init(document.querySelector(".carousel.varbase-heroslider", context));
       }));
     }
   };
-}(Drupal, once);
+})(Drupal, once, bootstrap);
