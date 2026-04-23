619022e4580217ab9a3832e7946d31043e4b82d5
Alamgir Hossain <71021-alamgir.hossain.cse@users.noreply.drupalcode.org>
2026-04-23 07:50:34 +0000

    feat: #3586182 Add AOS Animation Support for Components

---
+++ js/aos-init.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Initializes AOS (Animate On Scroll) as a Drupal behavior.
+ *
+ * AOS is loaded via the varbase_components/aos library, which is attached
+ * only when a component has an animation selected. This file runs after
+ * aos.js is available on window.AOS.
+ */
+(function (Drupal, once) {
+  'use strict';
+
+  Drupal.behaviors.varbaseComponentsAos = {
+    attach: function (context, settings) {
+      once('varbase-aos-init', 'html', context).forEach(function () {
+        if (typeof AOS !== 'undefined') {
+          AOS.init({
+            once: true,
+            disable: window.matchMedia('(prefers-reduced-motion: reduce)').matches,
+          });
+        }
+      });
+    },
+  };
+
+})(Drupal, once);
